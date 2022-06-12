3aa44891257cca9aec6eccf5563f93317b9065bd
Andrey Vedischev <vedischev76@mail.ru>
2022-06-12 20:37:42 +0500

    добавили пример 3

---
+++ пример3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите первое число: ");
+int number1 = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int number2 = int.Parse(ReadLine());
+
+Write(number1%number2==0?"Кратно": $"Не кратно, остаток = {number1%number2}.");
