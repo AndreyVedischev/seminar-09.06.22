45a05a2486d1063b7b24c94f7e8b4cf094e78bc8
Andrey Vedischev <vedischev76@mail.ru>
2022-06-12 20:31:37 +0500

    добавили пример 2

---
+++ пример2/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int number=new Random().Next(100,1000);
+System.Console.WriteLine($"Случайное число: {number} -> {(number/100)*10+number%10}");
