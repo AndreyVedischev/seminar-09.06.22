eda8055d6c8310c8d64f7dd532ab2eca1878db24
Andrey Vedischev <vedischev76@mail.ru>
2022-06-12 20:17:18 +0500

    добавили пример вывода чисел и строк с помощью $, и сравнение оператором answer

---
+++ пример1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int number=new Random().Next(10,100);
+//int number=88;
+WriteLine($"Случайное число =  {number}");
+
+string answer=number/10>number%10
+?$"Максимальное число = {number/10}"
+:number/10<number%10
+    ?$"Максимальное число = {number%10}"
+    :"Числа равны";
+
+WriteLine(answer);
